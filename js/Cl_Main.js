dafbe73a57034b8edb6dfac48ca007afc0aad2e1
SevenPip <157536274+SevenPip@users.noreply.github.com>
2024-04-14 12:25:16 -0400

    Initial commit

---
+++ js/Cl_Main.js
@@ -0,0 +1,14 @@
+/**
+ *¿cuánto se gastó en total durante los 3 meses?
+ *¿cuánto se gastó en educación en febrero?
+ *¿Cuál fue el gasto total en comida durante los primeros tres meses?
+ *¿Cuánto se gastó en mantenimiento en marzo?
+ *¿Cuál fue el gasto más alto en un solo día y en qué categoría fue?
+ *¿En qué categoría se gastó más dinero durante los tres meses?
+ *¿Cuántos días se gastó en entretenimiento durante febrero?
+ *¿Cuál fue el gasto promedio diario en servicios durante enero?
+ *¿Cuánto se gastó en vestido durante los tres meses?
+ *¿Cuál fue el gasto total en transporte durante los tres meses?
+ *¿En qué día se gastó más en educación durante marzo?
+ *¿Cuánto se gastó en total durante los tres meses en cada categoría?
+ */
